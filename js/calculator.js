4673fc0156b8fa2fb6265259ed36b50923719804
M Coindreau <mariannacoindreau@gmail.com>
2017-08-28 16:30:05 -0500

    fixed numbers into fields

--- js/calculator.js
+++ js/calculator.js
@@ -1,101 +1,28 @@
-"use strict";
+// "use strict";
 
-document.getElementById("c").addEventListener("click", function (){
-console.log(this)
-});
 
-document.getElementById("divide").addEventListener("click", function (){
-    console.log(this)
-});
+var result1 = document.getElementById("result1");
+var operator = document.getElementsByClassName("operator");
+var numKeys = document.getElementsByClassName("number");
+var result2 = document.getElementById("result2")
+var result3 = document.getElementById("result3");
 
-document.getElementById("add").addEventListener("click", function(){
-    console.log(this)
-});
 
-document.getElementById("seven").addEventListener("click", function(){
+for(var i = 0; i < numKeys.length; i++){
+    numKeys[i].addEventListener("click", function(){
 
-    console.log(this)
-});
+        if (result3.innerHTML === "" && result2.innerHTML === "") {
+            result1.innerHTML += this.innerHTML;
+        } else {
+            result3.innerHTML += this.innerHTML;
+        }
+        console.log(this.innerHTML);
+    })
+}
 
-document.getElementById("eight").addEventListener("click", function(){
-console.log(this)
-});
-
-document.getElementById("nine").addEventListener("click", function(){
-    console.log(this)
-});
-
-document.getElementById("sub").addEventListener("click", function(){
-    console.log(this)
-});
-
-document.getElementById("four").addEventListener("click", function(){
-    console.log(this)
-});
-document.getElementById("five").addEventListener("click", function(){
-    console.log(this)
-});
-document.getElementById("six").addEventListener("click", function(){
-    console.log(this)
-});
-document.getElementById("mult").addEventListener("click", function(){
-    console.log(this)
-});
-document.getElementById("one").addEventListener("click", function(){
-    console.log(this)
-});
-document.getElementById("two").addEventListener("click", function(){
-    console.log(this)
-});
-document.getElementById("three").addEventListener("click", function(){
-    console.log(this)
-});
-document.getElementById("slash").addEventListener("click", function(){
-    console.log(this)
-});
-document.getElementById("zero").addEventListener("click", function(){
-    console.log(this)
-});
-document.getElementById("period").addEventListener("click", function(){
-    console.log(this)
-});
-document.getElementById("equal").addEventListener("click", function(){
-    console.log(this)
-});
-
-window.onload = function() {
-
-    var current,
-        screen,
-        output,
-        limit,
-        zero,
-        period,
-        operator;
-
-    screen = document.getElementById("result1");
-
-    var number = document.querySelectorAll(".number");
-
-    var len = elem.length;
-
-    for (var i = 0; i < len; i++) {
-
-        screen[i].addEventListener("click", function () {
-
-            number = this.value;
-
-            output = screen.innerHTML +=len;
-
-            // limit = output.length;
-
-            if (limit > 16) {
-
-                alert("Sorry no more input is allowed");
-
-            }
-
-        }, false);
-
-    }
+for(var i = 0; i < operator.length; i++) {
+    operator[i].addEventListener("click", function(){
+        result2.innerHTML = this.innerHTML;
+        console.log(this);
+    })
 }
