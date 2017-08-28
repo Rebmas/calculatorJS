7c7c64d1a6531b071ac77f3f7a2963658b1ee9d4
becca <r.alexis.tolentino@gmail.com>
2017-08-28 17:00:06 -0500

    add functionality with operators in JS

--- js/calculator.js
+++ js/calculator.js
@@ -4,9 +4,10 @@
 var result1 = document.getElementById("result1");
 var operator = document.getElementsByClassName("operator");
 var numKeys = document.getElementsByClassName("number");
-var result2 = document.getElementById("result2")
+var result2 = document.getElementById("result2");
 var result3 = document.getElementById("result3");
-
+var clear = document.getElementById("c");
+var equal =document.getElementById("equal");
 
 for(var i = 0; i < numKeys.length; i++){
     numKeys[i].addEventListener("click", function(){
@@ -26,3 +27,38 @@ for(var i = 0; i < operator.length; i++) {
         console.log(this);
     })
 }
+
+clear.addEventListener("click", function(){
+    result1.innerHTML = "";
+    result2.innerHTML = "";
+    result3.innerHTML = "";
+});
+
+equal.addEventListener("click", function(){
+   if(result2.innerHTML === "*"){
+     result1.innerHTML = result1.innerHTML * result3.innerHTML
+   }
+   else if (result2.innerHTML === "-"){
+       result1.innerHTML = result1.innerHTML - result3.innerHTML
+   }
+    else if (result2.innerHTML === "/"){
+       result1.innerHTML = result1.innerHTML / result3.innerHTML
+   }
+   else if (result2.innerHTML === "+"){
+       result1.innerHTML = result1.innerHTML + result3.innerHTML
+   }
+   // else if (result2.innerHTML === "%"){
+   //     result1.innerHTML = result1.innerHTML  result3.innerHTML
+   // }
+
+
+    result2.innerHTML = "";
+    result3.innerHTML = "";
+
+
+
+});
+
+
+
+
